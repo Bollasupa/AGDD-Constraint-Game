74cd4e46c4221edc05bae7b2e11a80aa8807a7c5
Hauxii <haukur91@gmail.com>
2017-02-11 18:47:40 +0000

    StartScene added

---
+++ Assets/Scripts/ChangeSceneScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneScript : MonoBehaviour {
+	
+	public void ChangeSceneTo (string scene) {
+		//if (Input.GetKeyDown (KeyCode.Space)) {
+			//Scene level01 = SceneManager.GetSceneByName (scene);
+		SceneManager.LoadScene(scene);
+		//}
+	}
+
+	void Update(){
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			SceneManager.LoadScene("Level01");
+		}
+	}
+}
